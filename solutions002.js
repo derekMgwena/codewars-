ebb16db98024ea3236a33d815d4b0a9714028022
derekMgwena <derekmakhubela@gmail.com>
2022-05-19 21:26:22 +0200

    Complete the method that takes a boolean value and return a Yes string for true, or a No string for false.

---
+++ solutions002.js
@@ -0,0 +1,10 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+//P.R.E.P
+const bool = true;
+
+const str = bool === true ? 'Yes' : 'No';
+
+console.log(str); // 👉️ "Yes"
+
+
+
