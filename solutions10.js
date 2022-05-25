2abb49cb7db4d37f9d9ac16fd2d72e11f1212a3d
derekMgwena <derekmakhubela@gmail.com>
2022-05-25 10:45:35 +0200

    Implement a function which multiplies two numbers

---
+++ solutions10.js
@@ -0,0 +1,10 @@
+
+//Function 3 - multiplying two numbers
+//Implement a function which multiplies two numbers.
+
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+let resulMultiply = multiply(4, 5);
+console.log(resulMultiply);
