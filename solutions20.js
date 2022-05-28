532825f74f380de0c2994ce1cb4f91075ece58e4
derekMgwena <derekmakhubela@gmail.com>
2022-05-28 11:32:26 +0200

    Multiply the strings in the array

---
+++ solutions20.js
@@ -0,0 +1,12 @@
+
+
+//You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+//arrMultiply(['9','6']) should return '54'
+
+
+//Multiply the strings in the array
+function arrMultiply(arr){
+  let product =  arr[0] * arr[1];
+  return product.toString();
+}
