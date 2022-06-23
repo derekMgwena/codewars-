e8c912630b655b4cd47462a9514a5372a742a3cc
derekMgwena <derekmakhubela@gmail.com>
2022-06-23 10:52:10 +0200

    Creating a string for an array of objects from a set of words

---
+++ solutions93.js
@@ -0,0 +1,10 @@
+/*An array is said to be hollow if it contains 3 or more 0s in the middle that are preceded and followed by the same number of non-zero elements. Furthermore, all the zeroes in the array must be in the middle of the array.
+
+Write a function named isHollow/is_hollow/IsHollow that accepts an integer array and returns true if it is a hollow array,else false.*/
+const isHollow = (arr) => {
+  while (arr.length >= 3 && arr[0] !== 0 && arr[arr.length - 1] !== 0) {
+    arr.pop();
+    arr.shift();
+  }
+  return arr.length >= 3 && arr.every((num) => num === 0);
+};
